d476b8f5b509a97d85d0ebe1c2d91e5aedbbe65d
LarsSchnitzler <lars@schnitzler.ch>
2023-10-09 13:56:56 +0200

    blabla

--- javascript.js
+++ javascript.js
@@ -1,11 +1,3 @@
 import { supa } from "./SupaBaseClient/supabase.js";
 
 console.log("DatenBank_des_Projekts_Swapster_verbunden")
-
-//Alle User abrufen
-async function UserTest() {
-    const { data, error } = await supa.from("users").select();
-    return data;
-}
-
-console.log('UserTest', UserTest());
