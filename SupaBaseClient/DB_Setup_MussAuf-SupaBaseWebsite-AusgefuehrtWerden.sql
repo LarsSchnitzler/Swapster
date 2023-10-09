70a2a6a544f5edeab2d6d60aeb17e8d73c8ecacb
LarsSchnitzler <lars@schnitzler.ch>
2023-10-09 13:53:26 +0200

    blabla

--- SupaBaseClient/DB_Setup_MussAuf-SupaBaseWebsite-AusgefuehrtWerden.sql
+++ SupaBaseClient/DB_Setup_MussAuf-SupaBaseWebsite-AusgefuehrtWerden.sql
@@ -1,12 +1,3 @@
--- Create the users table
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    title INTEGER,
-    email VARCHAR(255) NOT NULL
-);
-
 -- Create the articles table
 CREATE TABLE articles (
     article_id SERIAL PRIMARY KEY,
@@ -16,14 +7,6 @@ CREATE TABLE articles (
     category VARCHAR(255) NOT NULL,
     user_id INTEGER REFERENCES users(user_id),
 );
-
--- Insert ExampleUsers
-INSERT INTO users (first_name, last_name, title, email)
-VALUES 
-('Max', 'Muster', 0, 'max@muster.ch'),
-('Bax', 'Buster', 1, 'bax@buster.ch'),
-('Cax', 'Custer', 2, 'cax@custer.ch')
-;
 
 -- Insert ExampleArticles
 INSERT INTO articles (title, price, upload_date, category, user_id)
