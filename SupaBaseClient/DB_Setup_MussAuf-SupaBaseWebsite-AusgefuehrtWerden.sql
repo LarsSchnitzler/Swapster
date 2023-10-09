aeec320d14e434e664aba8077a3ec5ae4357df8c
LarsSchnitzler <lars@schnitzler.ch>
2023-10-09 15:09:00 +0200

    blabla_sqlFile_change

--- SupaBaseClient/DB_Setup_MussAuf-SupaBaseWebsite-AusgefuehrtWerden.sql
+++ SupaBaseClient/DB_Setup_MussAuf-SupaBaseWebsite-AusgefuehrtWerden.sql
@@ -1,17 +1,36 @@
 -- Create the articles table
-CREATE TABLE articles (
-    article_id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    price INTEGER,
-    upload_date DATE,
-    category VARCHAR(255) NOT NULL,
-    user_id INTEGER REFERENCES users(user_id),
-);
+create table
+  articles (
+    article_id SERIAL primary key,
+    title varchar(255) not null,
+    price integer,
+    uploadDate DATE,
+    imgPath varchar(255) not null,
+    user_id integer references users (user_id)
+  );
 
 -- Insert ExampleArticles
-INSERT INTO articles (title, price, upload_date, category, user_id)
-VALUES 
-('', '', '', '', ''),
-('', '', '', '', ''),
-('', '', '', '', '')
-;
+insert into
+  articles (title, price, uploadDate, imgPath, user_id)
+values
+  (
+    'hose',
+    '20',
+    '2023-10-10',
+    './img/test1.jpg',
+    '1'
+  ),
+  (
+    'socken',
+    '10',
+    '2023-07-12',
+    './img/test2.jpg',
+    '2'
+  ),
+  (
+    'tshirt',
+    '15',
+    '2023-05-02',
+    './img/test3.jpg',
+    '3'
+  );
