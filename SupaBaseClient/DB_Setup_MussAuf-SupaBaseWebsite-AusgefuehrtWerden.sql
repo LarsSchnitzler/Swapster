5c9f985a685ec0424b3f4777979723ef3d3a8642
LarsSchnitzler <lars@schnitzler.ch>
2023-10-10 10:31:10 +0200

    DeleteWorkspaceFile

--- SupaBaseClient/DB_Setup_MussAuf-SupaBaseWebsite-AusgefuehrtWerden.sql
+++ SupaBaseClient/DB_Setup_MussAuf-SupaBaseWebsite-AusgefuehrtWerden.sql
@@ -6,31 +6,28 @@ create table
     price integer,
     uploadDate DATE,
     imgPath varchar(255) not null,
-    user_id integer references users (user_id)
+    --user_id integer references users (user_id)
   );
 
 -- Insert ExampleArticles
 insert into
-  articles (title, price, uploadDate, imgPath, user_id)
+  articles (title, price, uploadDate, imgPath)
 values
   (
     'hose',
     '20',
     '2023-10-10',
     './img/test1.jpg',
-    '1'
   ),
   (
     'socken',
     '10',
     '2023-07-12',
     './img/test2.jpg',
-    '2'
   ),
   (
     'tshirt',
     '15',
     '2023-05-02',
     './img/test3.jpg',
-    '3'
   );
