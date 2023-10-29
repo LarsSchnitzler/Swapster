255a55ef74a2c4e93cc513019ee33405b86812b9
LarsSchnitzler <lars@schnitzler.ch>
2023-10-29 17:58:07 +0100

    log in fix

--- js/javascript_log-in.js
+++ js/javascript_log-in.js
@@ -1,76 +1,41 @@
 import { supa } from "../SupaBaseClient/supabase.js";
 
-async function signInWithEmailAndPassword() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    console.log("read email and password");
+async function login() {
+    const email = document.getElementById('emailInput').value; 
+    const password = document.getElementById('passwordInput').value;
 
-    const { user, error } = await supa.auth.signIn({
-      email: email,
-      password: password
-    });
-  
+    const { user, error } = await supa.auth.signIn({ email, password });
+
     if (error) {
-      console.log('Error signing in:', error.message);
+        console.error("Error during login: ", error.message);
     } else {
-      console.log('Signed in successfully:', user);
+        console.log("Logged in as ", user.email);
     }
-  }
+
+    window.location.href='./dashboard.html';
+}
 
 function updateUserStatus(user) {
-    const parentDiv = document.querySelector('.wrapper_status');
-    const userStatusElement = document.querySelector('.status-signal');
-    const childDiv = document.querySelector('.wrapper_status .txt');
+    const userStatusElement = document.getElementById('userStatus');
 
-    if (user) {    
-        userStatusElement.style.backgroundColor = 'var(--lightGreen)';
-
-        //Creates div with userEmail in it
-        const newDiv = document.createElement('div');
-        newDiv.textContent = `The following UserEmail is logged in: ${user.email}`;
-        newDiv.className = 'txt';
-        parentDiv.insertBefore(newDiv, childDiv.nextSibling);
-
-        //Changes the loginButtons Content and href to Dashboard
-        const button = document.getElementById('LoginButton');
-        button.textContent = 'Go to dashboard';
-        button.setAttribute('href', './dashboard.html');
-        console.log('User logged in1');
-    } 
-    else {
-        userStatusElement.style.backgroundColor = 'var(--lightRed)';
-        console.log('No User logged in');
+    if (user) {
+        userStatusElement.textContent = `Authenticated as: ${user.email}`;
+    } else {
+        userStatusElement.textContent = "Not authenticated";
     }
 }
 
-//checks wether user is already in localStorage
 const initialUser = supa.auth.user();
 updateUserStatus(initialUser);
 
-//Makes it so that when LoginButton is pressed, supa.auth.signIn is executed
-document.getElementById('LoginButton').addEventListener('click', signInWithEmailAndPassword);
+document.getElementById('loginButton').addEventListener('click', login);
 
-/* Checks seemingly redunantly, trough the supa.auth.onAuthStateChange - Method, wether Authentication-State has changed. (could have been done in the signIn-Function instead) 
-This is done so that you don't have to execute updateUserStatus() in a logoutfunction, for example. */
 supa.auth.onAuthStateChange((event, session) => {
-  if (event === "SIGNED_IN") {
-      updateUserStatus(session.user);
-      console.log('User logged in0');
-  } else if (event === "SIGNED_OUT") {
-      updateUserStatus(null);
-  }
+    if (event === "SIGNED_IN") {
+        console.log("User signed in: ", session.user);
+        updateUserStatus(session.user);
+    } else if (event === "SIGNED_OUT") {
+        console.log("User signed out");
+        updateUserStatus(null);
+    }
 });
-
-//Eventlistener for SignIn (html contents need to be changed to sign on). Then the contents of sign-up.html should replace the whole body of the log in page using fetch()
-// 
-fetch('other-file.html')
-  .then(response => response.text())
-  .then(html => {
-    // Parse the HTML string into a DOM tree
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
-
-    // Replace the current HTML body with the body from the other file
-    document.body.innerHTML = doc.body.innerHTML;
-  })
-  .catch(error => console.log(error));
