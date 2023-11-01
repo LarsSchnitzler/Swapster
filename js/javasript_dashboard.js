12ac0245df15ec441bf5ab03220c4176600925c7
LarsSchnitzler <lars@schnitzler.ch>
2023-11-01 17:00:32 +0100

    lars

--- js/javasript_dashboard.js
+++ js/javasript_dashboard.js
@@ -1,49 +1,39 @@
 import { supa } from "../SupaBaseClient/supabase.js";
 
-Read (Price-Tolerance and Selected OwnItem) from Localstorage(In case of first execution of Reading Matches in a session, assume standard-tolerance and assume first OwnItem is selected -> DB query)
+/* function: setArticle (articles, articleIndex, divIdImg, divIdCaption, divIdTitle) {
+    const ownItemsDiv = document.getElementById(".ownItems-div");
+    ownItemsDiv.style.backgroundimage(url:"articles."+ articleIndex +".imgPath")
+}*/
+const { user } = supa.auth.user();
 
-/* function dashboard () {
-    OwnArticles = Read all articles where userID = supa.auth.user.userID
+const { data: ownArticles, error: ownArticlesError } = await supa
+    .from("articles")
+    .select("*")
+    .eq("userID", user.id);
+    .join("IMGtable", { foreignKey: "articleID" });
 
-    Read PriceTolerance from html form //if not set, use standard-tolerance of 5 chf
-    Read() from articles where price is < PriceTolerance && UploadUserId != userID JOIN IMGPaths from IMGtable where IMGarticleID = articleID
+const ownArticleIndex = 0;
+const matchingArticleIndex = 0;
+const priceTolerance = document.getElementById("price-tolerance").value || 5;
 
-    fill corresponding ownItems-div with caption from articles and background image with IMGPaths
+const Element_ownArticle_img = document.getElementById("");
+const Element_matchingArticle_img = document.getElementById("next-button");
 
-    OwnArticles = Read all articles where userID = supa.auth.user.userID
+const { data: matchingArticles, error: matchingArticlesError } = await supabase
+    .from("articles")
+    .select("*")
+    //.selfjoin("articles", {price = price - priceTolerance || price = price + priceTolerance});
+    //???: .lt("price", priceTolerance)
+    .neq("userID", user.id)
 
-    Read PriceTolerance from html form //if not set, use standard-tolerance of 5 chf
-    Read() from articles where price is < PriceTolerance && UploadUserId != userID JOIN IMGPaths from IMGtable where IMGarticleID = articleID
+//setArticle(ownArticles, ownArticleIndex, )
 
-    fill corresponding ownItems-div with caption from articles and background image with IMGPaths
+ownItemsDivs.forEach((div, index) => {
+    div.style.backgroundImage = `url(${matchingArticles[index].IMGPaths})`;
+    div.textContent = matchingArticles[index].caption;
+});
 
-    Eventlistener for Click on "next" 
-} */
-
-function dashboard () {
-    const { user } = supa.auth.user();
-    const { data: ownArticles, error: ownArticlesError } = await supa
-        .from("articles")
-        .select("*")
-        .eq("userID", user.id);
-
-    const priceTolerance = document.getElementById("price-tolerance").value || 5;
-    const { data: matchingArticles, error: matchingArticlesError } = await supabase
-        .from("articles")
-        .select("caption, IMGPaths")
-        .lt("price", priceTolerance)
-        .neq("userID", user.id)
-        .join("IMGtable", { foreignKey: "articleID" });
-
-    const ownItemsDivs = document.querySelectorAll(".ownItems-div");
-
-    ownItemsDivs.forEach((div, index) => {
-        div.style.backgroundImage = `url(${matchingArticles[index].IMGPaths})`;
-        div.textContent = matchingArticles[index].caption;
-    });
-
-    const nextButton = document.getElementById("next-button");
-    nextButton.addEventListener("click", () => {
-        // code for handling click on "next" button
-    });
-}
+const nextButton = document.getElementById("next-button");
+nextButton.addEventListener("click", () => {
+    // code for handling click on "next" button
+});
