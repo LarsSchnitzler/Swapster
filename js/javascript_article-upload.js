c8cb9d4c48b6b2f0adcf59e529b2d64c6e6aa721
LarsSchnitzler <lars@schnitzler.ch>
2023-11-04 10:19:00 +0100

    lars

---
+++ js/javascript_article-upload.js
@@ -0,0 +1,68 @@
+import { supa } from "../SupaBaseClient/supabase.js";
+
+async function insertArtInf_articles(parameterImgPath, parameterTitle, parameterCaption, parameterPrice, parameterobjectId) {
+    console.log('uploadArticleInfo');
+
+    const user = supa.auth.user();
+    const userId = user ? user.id : null;
+
+    const {error: articleInsertError} = await supa.from('articles').insert([
+        {
+            user_id: userId,
+            img_path: parameterImgPath,
+            title: parameterTitle,
+            caption: parameterCaption,
+            price: parameterPrice,
+            object_id: parameterobjectId
+        }
+    ]);
+    
+    if (articleInsertError) {
+        console.error('Error saving to "articles" table:', articleInsertError.message);
+    } else {
+        console.log('ArticleInfo Insert successfull:');
+    } 
+}
+
+async function uploadPhoto(bucketName) {
+    const fileInput = document.getElementById('file-upload');
+    const inputValue_title = document.getElementById('articleCaption').value;
+    const inputValue_caption = document.getElementById('articleCaption').value;
+    const inputValue_price = document.getElementById('estValue').value;
+    console.log('uploadPhoto');
+
+    if (fileInput.files.length > 0) {
+        const file = fileInput.files[0];
+        const filePath = `uploads/${file.name}`;
+        const { data, error: uploadError } = await supa.storage.from(bucketName).upload(filePath, file);
+        
+        if (uploadError) {
+            console.error('Error uploading file:', uploadError);
+            return;
+        } else{
+            console.log('File uploaded successfully');
+        }
+
+        const objectId = data.id;
+        console.log('objectId: ', objectId);
+
+        insertArtInf_articles(filePath, inputValue_title, inputValue_caption, inputValue_price);
+
+    } else {
+        console.log('No file selected.');
+    }
+}
+
+const { user, error } = await supa.auth.signIn({
+    email: 'lars@schnitzler.ch',
+    password: 'abc.123',
+});
+
+if (error) {
+    console.error('Error signing in:', error)
+}else{
+    console.log('sign in successful:', user)
+}
+
+const uploadButton = document.getElementById('articleUpload_Upload');
+uploadButton.addEventListener('click', function() {uploadPhoto('article_img');});
